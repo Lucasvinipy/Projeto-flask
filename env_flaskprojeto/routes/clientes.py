d3185a455bdd9dcfe01b5584a4bdd5fdee248e2f
lucas vinicius reis de oliveira <lucasvini6@hotmail.com>
2025-01-21 14:39:14 -0300

    atualiaçoes do CRUD

--- env_flaskprojeto/routes/clientes.py
+++ env_flaskprojeto/routes/clientes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint , render_template
+from flask import Blueprint , render_template , request
 from database.cliente import CLIENTES 
 
 cliente_route = Blueprint('cliente',__name__)
@@ -19,8 +19,18 @@ def lista_cliente():
 
 
 @cliente_route.route('/', methods = ['POST'])
-def inserir_cliente():
-   pass
+def inserir_cliente():  
+   data = request.json
+
+   novo_usuario = {
+      'id': len(CLIENTES) + 1,
+      "nome" : data['nome'],
+      "email": data['email'   ],
+
+   }
+
+   CLIENTES.append(novo_usuario)
+   return render_template('item_cliente.html', cliente = novo_usuario)
 
 
 @cliente_route.route('/new')    
@@ -29,19 +39,42 @@ def Form_cliente():
 
 
 @cliente_route.route('/<int:cliente_id>')
-def Detalhe_cliente(cliente_id):
-   return render_template('Detalhe_cliente.html')
+def detalhe_cliente(cliente_id):
+
+   cliente = list(filter(lambda c: c['id'] == cliente_id,CLIENTES))[0]
+   return render_template('detalhe_cliente.html', cliente = cliente)
 
 
 @cliente_route.route('/<int:cliente_id>/edit')
 def form_edit_cliente(cliente_id):
-   return render_template('form_edit_cliente.html')
+
+   cliente= None
+   for c in CLIENTES:
+      if c['id'] == cliente_id :
+         cliente = c
+   return render_template('form_cliente.html', cliente=cliente)
 
 
 @cliente_route.route('/<int:cliente_id>/update', methods = ['PUT'])
-def update_cliente(cliente_id):
-   pass 
+def atualizar_cliente(cliente_id):
+
+   cliente_editado=None
+
+   data = request.json
+
+   for c in  CLIENTES:
+      if c['id'] == cliente_id:
+         c['nome'] = data['nome']
+         c['email'] = data['email']
+
+         cliente_editado = c
+
+
+   return render_template('item_cliente.html',cliente= cliente_editado) 
 
 @cliente_route.route('/<int:cliente_id>/delete', methods = ['DELETE'])
 def deletar_cliente(cliente_id):
-   pass 
+   global CLIENTES
+   CLIENTES = [ c for c in CLIENTES if c['id'] != cliente_id]
+
+   return{'deleted' : 'ok '}
