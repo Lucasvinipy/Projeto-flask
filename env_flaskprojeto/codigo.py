d9d51cb2378e5682c018272e69cfb345ae78a9c1
lucas vinicius reis de oliveira <lucasvini6@hotmail.com>
2025-01-22 14:44:03 -0300

    banco de dados

--- env_flaskprojeto/codigo.py
+++ env_flaskprojeto/codigo.py
@@ -1,9 +1,8 @@
 from flask import Flask 
-from routes.home import home_route
-from routes.clientes import cliente_route
+from configuration import configure_all
 
 app = Flask(__name__)
-app.register_blueprint(home_route)
-app.register_blueprint(cliente_route, url_prefix = '/clientes')
 
-app.run(debug=True)
+configure_all(app)
+
+app.run(debug=True)         
